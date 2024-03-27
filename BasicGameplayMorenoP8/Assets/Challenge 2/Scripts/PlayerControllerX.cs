d5965a5869ea06a3d9e8de028f3f24ac4973a935
Rodrigo Moreno <750694@student.dusd.net>
2024-03-27 14:48:52 -0700

    Finished the challenge
    I finished the challenge game that I have created in my Unity Unit 2 Basic Gameplay

---
+++ BasicGameplayMorenoP8/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    public float reloadTime = 1.0f;
+    public bool reloaded = true;
+
+    void Start()
+    {
+        InvokeRepeating("ReloadProjectile", reloadTime, reloadTime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && reloaded == true)
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            reloaded = false;
+        }
+    }
+
+    void ReloadProjectile()
+    {
+        reloaded = true;
+    }
+}
